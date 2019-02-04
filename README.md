57d872acbecfec04134b9e3bf5f4bd8880d50942
Luis Ricardo <luisricardomancilla@hotmail.com>
2019-02-04 11:35:19 -0500

    Added Reactive-Form

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.3.
 
+In this project I am using both Template-Driven-Forms and Reactive-Forms.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html'
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent {
+  myForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
   }
 
+  onSubmit(value: string){
+    console.log('You submitted value:', value);
+  }
 }
